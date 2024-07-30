90b7f2f9cafd419b9e728cda6e4e498db3c0739f
hamza-m-farooqi <hamzamfarooqi@gmail.com>
2024-07-30 09:30:07 +0000

    flask serever revision for sdxl 1

--- flask_server_lora.py
+++ flask_server_lora.py
@@ -1,23 +1,32 @@
-from flask import Flask, request, jsonify
-from pydantic import BaseModel, Field, conint
-from typing import List
 import os
+import re
 import uuid
+import toml
+import threading
+import subprocess
+import select
+import shutil
+from typing import List
+from io import BytesIO
+from flask import Flask, request, jsonify,send_file
+from pydantic import BaseModel, Field, conint
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
-import toml
-import subprocess
-from io import BytesIO
-
+from decouple import config
 app = Flask(__name__)
 
+
+PRETRAINED_SDXL_MODEL_PATH=config("PRETRAINED_SDXL_MODEL_PATH")
+LORA_TRAINING_DATASET_PATH=config("LORA_TRAINING_DATASET_PATH")
+PROJECT_DEPLOYMENT_PATH=config("PROJECT_DEPLOYMENT_PATH")
+TRAINED_LORA_FINAL_DESTINATION=config("TRAINED_LORA_FINAL_DESTINATION")
 # Pydantic Model for Request
 class TrainingRequest(BaseModel):
     lora_name: str
     learning_rate: float = 4e-7
     max_training_steps: conint(gt=0) = 1600
     network_dim: conint(gt=0) = 128
-    network_alpha: conint(gt=0) = 128
+    network_alpha: conint(gt=0) = 64
 
 # Pydantic Model for Config
 class Config(BaseModel):
@@ -29,7 +38,7 @@ class Config(BaseModel):
     dynamo_backend: str = "no"
     enable_bucket: bool = True
     epoch: int = 1
-    gradient_accumulation_steps: int = 4
+    gradient_accumulation_steps: int = 6
     huber_c: float = 0.1
     huber_schedule: str = "snr"
     learning_rate: float = 4e-7
@@ -59,7 +68,8 @@ class Config(BaseModel):
     optimizer_type: str = "AdamW8bit"
     output_dir: str = ""
     output_name: str = "last"
-    pretrained_model_name_or_path: str = "/var/www/projects/FOOCUS-Django/REPOSITORIES/Fooocus/models/checkpoints/epicrealismXL_V7FinalDestination.safetensors"
+    pretrained_model_name_or_path: str = PRETRAINED_SDXL_MODEL_PATH
+    # pretrained_model_name_or_path: str = "runwayml/stable-diffusion-v1-5"
     prior_loss_weight: int = 1
     resolution: str = "1024,1024"
     sample_prompts: str = ""
@@ -76,9 +86,9 @@ class Config(BaseModel):
 # Create necessary folders and save images
 def create_folders_and_save_images(files, lora_name):
     request_id = str(uuid.uuid4())
-    base_path = f"/var/www/projects/lora_datasets/{request_id}_{lora_name}"
+    base_path = f"{LORA_TRAINING_DATASET_PATH}{request_id}_{lora_name}"
     images_path = os.path.join(base_path, "images")
-    images_save_path= os.path.join(images_path,f"100_{lora_name}.safetensors")
+    images_save_path= os.path.join(images_path,f"100_{lora_name}")
     models_path = os.path.join(base_path, "models")
     logs_path = os.path.join(base_path, "logs")
 
@@ -110,6 +120,92 @@ def generate_captions(images_path, lora_name):
         caption_file = os.path.join(images_path, image_file.replace(".jpg", ".txt"))
         with open(caption_file, "w") as f:
             f.write(caption)
+
+def get_progress_percentage(output_line):
+    # Regex pattern to find the diffusion process progress
+    pattern = re.compile(r'(\d+)/(\d+)\s+\[')
+    match = pattern.search(output_line)
+    print(match)
+    if match:
+        current, total = map(int, match.groups())
+        percentage = (current / total) * 100
+        return percentage
+    return None
+
+
+def background_training(toml_path,config:Config,training_request:TrainingRequest):
+    project_deployment_path=PROJECT_DEPLOYMENT_PATH
+    # command = f"bash -c 'source {project_deployment_path}/venv/bin/activate && cd {project_deployment_path}/  && python sdxl_train.py --config {toml_path}'"
+    command = f"bash -c 'source {project_deployment_path}/venv/bin/activate && cd {project_deployment_path}/  && accelerate launch --dynamo_backend no --dynamo_mode default --mixed_precision fp16 --num_processes 1 --num_machines 1 --num_cpu_threads_per_process 2 sdxl_train_network.py --config {toml_path}'"
+    print(command)
+
+    try:
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        # Use select to read from stdout and stderr without blocking
+        while True:
+            reads = [process.stdout.fileno(), process.stderr.fileno()]
+            ret = select.select(reads, [], [])
+            for fd in ret[0]:
+                if fd == process.stdout.fileno():
+                    read = process.stdout.readline()
+                    if read:
+                        output = read.strip()
+                        print(output)
+                        percentage=0
+                        if f"/{config.max_train_steps}"  in output:
+                            percentage_value = get_progress_percentage(output)
+                            percentage = percentage_value if percentage_value else 0
+                        # job.job_progress = percentage
+                if fd == process.stderr.fileno():
+                    read = process.stderr.readline()
+                    if read:
+                        output = read.strip()
+                        print(output)
+                        percentage=0
+                        if f"/{config.max_train_steps}" in output:
+                            percentage_value = get_progress_percentage(output)
+                            percentage = percentage_value if percentage_value else 0
+                        # job.job_progress = percentage
+
+            if process.poll() is not None:
+                break
+
+        return_code = process.poll()
+        if return_code != 0:
+            raise subprocess.CalledProcessError(return_code, command)
+
+        print("Job is Finished")
+        trained_model_path=os.path.join(config.output_dir,f"{config.output_name}.{config.save_model_as}")
+        if not os.path.exists(trained_model_path):
+            print("Trained Model Not Found!")
+        # MOVE TRAINED LORA MODEL FROM trained_model_path to TRAINED_LORA_FINAL_DESTINATION
+        shutil.move(trained_model_path, TRAINED_LORA_FINAL_DESTINATION)
+        print("Trained Lora Model Moved to: ", TRAINED_LORA_FINAL_DESTINATION)
+
+        # job.job_status = "finished"
+        # job.job_result = os.path.join(
+        #     settings.STATIC_URL,
+        #     "viton",
+        #     "outputs",
+        #     os.path.basename(job_params.output_path),
+        # )
+
+    except subprocess.CalledProcessError as e:
+        print(e)
+        # job.job_status = "failed"
+        # job.error_message = str(e)
+
+    except Exception as e:
+        print(e)
+        # job.job_status = "failed"
+        # job.error_message = str(e)
 
 @app.route('/train', methods=['POST'])
 def train():
@@ -143,6 +239,7 @@ def train():
         network_alpha=training_request.network_alpha,
         train_data_dir=images_path,
         output_dir=models_path,
+        output_name=lora_name,
         logging_dir=logs_path,
         sample_prompts=os.path.join(models_path, "prompt.txt")
     )
@@ -152,21 +249,15 @@ def train():
         toml.dump(config.dict(), toml_file)
 
     # Run training command
-    command = f"python train_network.py --config {toml_path}"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    def process_output(process):
-        while True:
-            output = process.stdout.readline()
-            if output == b"" and process.poll() is not None:
-                break
-            if output:
-                print(output.strip().decode())
-
-    process_output(process)
-    process.wait()
-
+    threading.Thread(target=background_training,args=(toml_path,config,training_request)).start()
     return jsonify({"status": "Training started", "config": config.dict()}), 200
+
+@app.route('/download', methods=['GET'])
+def download():
+    path = request.args.get('path')
+    if not os.path.exists(path):
+        return jsonify({"error": "File not found"}), 404
+    return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
